9543bf2a77028ce4e410a33cc37da8e113bc5ff1
AngellahKyazze <angellakyazze8@gmail.com>
2024-09-12 17:44:18 +0300

    Add files via upload

---
+++ Angellah/NameENAccessno.js
@@ -0,0 +1,5 @@
+
+//1ST JS APPLICATON
+console.log("HELLO WORLD");
+console.log("ANGELLAH KYAZZE");
+console.log("B28340");
---
+++ Angellah/Variables.js
@@ -0,0 +1,40 @@
+
+//vARIABLES
+
+Console.log ("Hello World");
+
+//Initialisation
+var my_name = "ANGELLAH KYAZZE";
+
+//Declare a variable
+var height;
+
+//Assign a var height
+height  = 3.14;
+
+
+//printing name EN height in one line
+console.log(my_name,"-",height);
+
+//reading the nmes
+console.log(my_name);
+console.log(height)
+
+
+//LET
+let area = 20.3;
+
+
+let f_name;
+
+f_name="ANGELLAH";
+
+
+//CONST
+//INTIALISE A VARIABLE
+const pie = 3.14;
+
+
+
+
+
