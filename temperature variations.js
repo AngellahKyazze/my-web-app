91d2610af35958de8bf3fcda116b1ebc31185cdb
AngellahKyazze <angellakyazze8@gmail.com>
2024-10-11 20:18:32 +0300

    Add files via upload

---
+++ temperature variations.js
@@ -0,0 +1,20 @@
+/////temperature
+let temperature = 34;
+if (temperature>23){
+    console.log("Its a hot day!");
+}else{
+    console.log("Tts a cold day!");
+}
+//The reason it displays that its because 
+
+
+
+///temperature 2
+let temp = 16;
+if(temp>25){
+    console.log("Its a hot day!");
+}else if(temp>15 &&temp<25){
+    console.log("Its a moderate day")
+}else{
+    console.log("Its a cold day")
+}
