018a7b6f1324bb699540accc8c55092be0591d67
AngellahKyazze <angellakyazze8@gmail.com>
2024-10-11 20:17:59 +0300

    Add files via upload

---
+++ differentkeywords.js
@@ -0,0 +1,16 @@
+//variable that stores the student name and no of students in the IT class
+var Name = "Angellah";
+const no_of_students = 123;
+
+
+console.log("Name of the student is :",Name, "no_of_students is :",no_of_students);
+
+
+/////temperature
+let temperature = 34;
+if (temperature>23){
+    console.log("Its a hot day!");
+}else{
+    console.log("Tts a cold day!");
+}
+
